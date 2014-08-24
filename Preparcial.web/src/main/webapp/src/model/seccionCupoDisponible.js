66e2692fe82ddbe1bbc3a67373669ee6086bff62
estudiante <estudiante@ISIS2603-21.virtual.uniandes.edu.co>
2014-08-23 23:42:36 -0500

    Requerimiento 5

---
+++ Preparcial.web/src/main/webapp/src/model/seccionCupoDisponible.js
@@ -0,0 +1,18 @@
+define(['model/seccionCupoDisponible'], function() {
+//Aquí se define la estructura de un ítem de la lista. Note que el modelo extiende el modelo estándar backbone.
+    App.Model.SportPromedioModel = Backbone.Model.extend({
+        defaults: {
+         'name' : '',   
+         'cupo' : ''
+        },
+        getDisplay: function(name) {
+         return this.get(name);
+        }
+        });
+//Aquí se define el modelo de la lista. El modelo de la lista extiende de Backbone.Collection. En el atributo ‘model’ se define el modelo  (definido arriba) que corresponde al molde de cada uno de los ítems de la lista.
+    App.Model.SeccionCupoDisponibleList = Backbone.Collection.extend({
+        model: App.Model.SeccionCupoDisponibleModel
+    });
+    return  App.Model.SeccionCupoDisponibleModel;
+});
+
